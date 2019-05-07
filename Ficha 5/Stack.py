65e8b7d6c98bd33b74f5047a7786242bd860ae61
Diogo Manuel Coelho Morais dos Santos <moozdzn@gmail.com>
2019-05-07 12:59:36 +0100

    Update Stack.py

--- Ficha 5/Stack.py
+++ Ficha 5/Stack.py
@@ -6,12 +6,59 @@ class Stack:
     def isEmpty(self):
         return self.items.isEmpty()
     def push(self,item):
-        self.items.append(item)
+        self.items.add(item)
     def pop(self):
-        return self.items.pop()
+        return self.items.pop(0)
     def peek(self):
         temp = self.items.pop()
         self.items.append(temp)
         return temp
     def size(self):
         return self.items.size()
+
+def test_stack():
+    print("CREATING EMPTY STACK")
+    s = Stack()
+    print(s, "\n")
+    print("The stack is empty?", s.isEmpty())
+    print("Stack size:", s.size())
+
+    push_items = [1, 2, 'dog', 4, 5]
+    print("\nPUSHING ITEMS", push_items)
+    for item in push_items:
+        #print("Adding item", item, end="   ")
+        s.push(item)
+        #print("my_list =", my_list)
+
+    print(s)
+    print("The stack is empty?", s.isEmpty())
+    print("Stack size:", s.size())
+    print("Item on the top:", s.peek())
+
+    print("\nPOPPING 2 ITEMS")
+    s.pop()
+    s.pop()
+    print(s)
+    print("The stack is empty?", s.isEmpty())
+    print("Stack size:", s.size())
+    print("Item on the top:", s.peek())
+
+    item = 'cat'
+    print("\nPUSHING ITEM", item)
+    s.push(item)
+    print(s)
+    print("The stack is empty?", s.isEmpty())
+    print("Stack size:", s.size())
+    print("Item on the top:", s.peek())
+
+    print("\nPOPPING ALL ITEMS")
+    for i in range(s.size()):
+        s.pop()
+
+    print(s)
+    print("The stack is empty?", s.isEmpty())
+    print("Stack size:", s.size())
+
+
+if __name__ == "__main__":
+    test_stack()
